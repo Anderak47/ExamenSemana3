cc070ccc7a330af8567372d44f028e4fd5c615ae
Anderak47 <N00220620@upn.pe>
2023-04-10 01:06:00 -0500

    Initial

---
+++ Assets/Script/PersonajeController.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonajeController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    private int currentAnimation = 1;
+    SpriteRenderer sr;
+    public float jumForce = 400f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        currentAnimation = 1;
+        var velocityY = rb.velocity.y;
+        rb.velocity = new Vector2(0, velocityY);
+        //derecha
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            currentAnimation = 2;
+            rb.velocity = new Vector2(5, velocityY);
+            sr.flipX = false;
+
+        }
+        //correr
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.X))
+        {
+            currentAnimation = 2;
+            rb.velocity = new Vector2(20, velocityY);
+            sr.flipX = false;
+
+        }
+        //izquierda
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            currentAnimation = 2;
+            rb.velocity = new Vector2(-5, velocityY);
+            sr.flipX = true;
+
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.X))
+        {
+            currentAnimation = 2;
+            rb.velocity = new Vector2(-20, velocityY);
+            sr.flipX = true;
+
+        }
+        //deslizar
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            bool verificar = sr.flipX;
+            if (verificar == true && Input.GetKey(KeyCode.D))
+            {
+                currentAnimation = 4;
+                rb.velocity = new Vector2(-5, velocityY);
+            }
+            if (verificar == false && Input.GetKey(KeyCode.D))
+            {
+                currentAnimation = 4;
+                rb.velocity = new Vector2(5, velocityY);
+            }
+        }
+        //ataque
+        if (Input.GetKey(KeyCode.B))
+        {
+            currentAnimation = 5;
+            rb.velocity = new Vector2(0, velocityY);
+
+        }
+        //TIRAR
+        if (Input.GetKey(KeyCode.T))
+        {
+            currentAnimation = 3;
+            rb.velocity = new Vector2(0, velocityY);
+        }
+        //muerte
+        if (Input.GetKey(KeyCode.M))
+        {
+            currentAnimation = 6;
+            rb.velocity = new Vector2(0, velocityY);
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(transform.up * jumForce);
+
+        }
+        animator.SetInteger("Estado", currentAnimation);
+    }
+}
